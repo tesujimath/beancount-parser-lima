b22a8d4eacbdb938e511cefd575d33026a801beb
Simon Guest <simon.guest@tesujimath.org>
2023-05-30 15:34:43 +1200

    anyhow

--- examples/runit.rs
+++ examples/runit.rs
@@ -1,6 +1,14 @@
+use anyhow::Result;
 use beancount_parser::add;
+use std::env::current_dir;
 
-fn main() {
+fn main() -> Result<()> {
     let result = add(1, 2);
-    println!("Hello from runit, result is {}", result)
+    println!(
+        "Hello from runit, result is {}, running in {}",
+        result,
+        current_dir()?.display()
+    );
+
+    Ok(())
 }
