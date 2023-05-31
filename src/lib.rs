b51d28b8751e2c1798955cae9b6a4734ab4af4ed
Simon Guest <simon.guest@tesujimath.org>
2023-05-31 12:32:45 +1200

    Progress with date parser

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,66 @@
 use chrono::NaiveDate;
-use nom::{bytes::complete::take, combinator::map_res, IResult};
+use nom::{
+    bytes::complete::{tag, take, take_while_m_n},
+    combinator::map_res,
+    error::{ErrorKind, FromExternalError},
+    IResult,
+};
+use std::{
+    error::Error,
+    fmt::{self, Display, Formatter},
+};
 
-pub fn date(i: &str) -> IResult<&str, NaiveDate> {
+#[derive(Debug)]
+enum ParseErrorReason {
+    DateOutOfRange,
+}
+
+#[derive(Debug)]
+pub struct ParseError {
+    reason: ParseErrorReason,
+}
+
+impl Display for ParseError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        match self.reason {
+            ParseErrorReason::DateOutOfRange => write!(f, "date out of range"),
+        }
+    }
+}
+
+impl Error for ParseError {}
+
+pub fn date(i0: &str) -> IResult<&str, NaiveDate> {
+    let (i, year) = map_res(
+        take_while_m_n(4, 4, |c: char| c.is_ascii_digit()),
+        |s: &str| s.parse::<i32>(),
+    )(i0)?;
+    let (i, _) = tag("-")(i)?;
+    let (i, month) = map_res(
+        take_while_m_n(2, 2, |c: char| c.is_ascii_digit()),
+        |s: &str| s.parse::<u32>(),
+    )(i)?;
+    let (i, _) = tag("-")(i)?;
+    let (i, day) = map_res(
+        take_while_m_n(2, 2, |c: char| c.is_ascii_digit()),
+        |s: &str| s.parse::<u32>(),
+    )(i)?;
+
+    match NaiveDate::from_ymd_opt(year, month, day) {
+        Some(d) => Ok((i, d)),
+        // TODO build this via helper
+        None => Err(nom::Err::Error(nom::error::Error::from_external_error(
+            i0, // TODO this is wrong, needs to be original i not final
+            ErrorKind::Fail,
+            ParseError {
+                reason: ParseErrorReason::DateOutOfRange,
+            },
+        ))), //None => Err(parse_error(i, ParseErrorReason::DateOutOfRange)),
+    }
+}
+
+pub fn date2(i: &str) -> IResult<&str, NaiveDate> {
+    // this isn't as good, as we lean too heavily on parse_from_str
     map_res(take(10usize), |s| NaiveDate::parse_from_str(s, "%Y-%m-%d"))(i)
 }
 
--- src/tests.rs
+++ src/tests.rs
@@ -1,11 +1,26 @@
 #![cfg(test)]
 use super::*;
-use nom::Finish;
+use nom::{
+    error::{Error, ErrorKind},
+    Err, IResult,
+};
+
 use test_case::test_case;
 
-#[test_case("2023-03-23", Some(("", NaiveDate::from_ymd_opt(2023, 3, 23).unwrap())))]
-#[test_case("2023-04-011", Some(("1", NaiveDate::from_ymd_opt(2023, 4, 1).unwrap())))]
-#[test_case("2023-03-XX", None)]
-fn test_date(s: &str, expected: Option<(&str, NaiveDate)>) {
-    assert_eq!(date(s).finish().ok(), expected);
+#[test_case("2023-03-23", Ok(("", NaiveDate::from_ymd_opt(2023, 3, 23).unwrap())))]
+#[test_case("2023-04-011", Ok(("1", NaiveDate::from_ymd_opt(2023, 4, 1).unwrap())))]
+#[test_case(
+    "2023-03-99",
+    Err(Err::Error(Error::new("2023-03-99", ErrorKind::Fail)))
+)]
+#[test_case(
+    "2023-03-XX",
+    Err(Err::Error(Error::new("XX", ErrorKind::TakeWhileMN)))
+)]
+#[test_case(
+    "freddyfish",
+    Err(Err::Error(Error::new("freddyfish", ErrorKind::TakeWhileMN)))
+)]
+fn test_date(s: &str, expected: IResult<&str, NaiveDate>) {
+    assert_eq!(date(s), expected);
 }
