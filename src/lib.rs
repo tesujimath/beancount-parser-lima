bf0a010aba39491d7f049471cfa732c595c27e3a
Simon Guest <simon.guest@tesujimath.org>
2023-05-31 15:47:40 +1200

    Using nom_supreme, but my tests are a bit crass

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,13 @@
 use chrono::NaiveDate;
 use nom::{
-    bytes::complete::{tag, take, take_while_m_n},
+    bytes::complete::{take, take_while_m_n},
     combinator::map_res,
-    error::{ErrorKind, FromExternalError},
     IResult,
+};
+use nom_supreme::{
+    error::{BaseErrorKind, ErrorTree},
+    final_parser::final_parser,
+    tag::complete::tag,
 };
 use std::{
     error::Error,
@@ -30,7 +34,7 @@ impl Display for ParseError {
 
 impl Error for ParseError {}
 
-pub fn date(i0: &str) -> IResult<&str, NaiveDate> {
+pub fn date(i0: &str) -> IResult<&str, NaiveDate, ErrorTree<&str>> {
     let (i, year) = map_res(
         take_while_m_n(4, 4, |c: char| c.is_ascii_digit()),
         |s: &str| s.parse::<i32>(),
@@ -49,19 +53,34 @@ pub fn date(i0: &str) -> IResult<&str, NaiveDate> {
     match NaiveDate::from_ymd_opt(year, month, day) {
         Some(d) => Ok((i, d)),
         // TODO build this via helper
-        None => Err(nom::Err::Error(nom::error::Error::from_external_error(
-            i0, // TODO this is wrong, needs to be original i not final
-            ErrorKind::Fail,
-            ParseError {
+        None => Err(nom::Err::Error(ErrorTree::Base {
+            location: i0,
+            kind: BaseErrorKind::External(Box::new(ParseError {
                 reason: ParseErrorReason::DateOutOfRange,
-            },
-        ))), //None => Err(parse_error(i, ParseErrorReason::DateOutOfRange)),
+            })),
+        })), //None => Err(parse_error(i, ParseErrorReason::DateOutOfRange)),
     }
 }
 
-pub fn date2(i: &str) -> IResult<&str, NaiveDate> {
+pub fn date2(i: &str) -> IResult<&str, NaiveDate, ErrorTree<&str>> {
     // this isn't as good, as we lean too heavily on parse_from_str
     map_res(take(10usize), |s| NaiveDate::parse_from_str(s, "%Y-%m-%d"))(i)
+}
+
+#[derive(PartialEq, Eq, Debug)]
+pub struct Doc {
+    d1: NaiveDate,
+    d2: NaiveDate,
+}
+
+pub fn doc(i: &str) -> IResult<&str, Doc, ErrorTree<&str>> {
+    let (i, d1) = date(i)?;
+    let (i, d2) = date(i)?;
+    Ok((i, Doc { d1, d2 }))
+}
+
+pub fn final_doc(i: &str) -> Result<Doc, ()> {
+    final_parser(doc)(i)
 }
 
 mod tests;
--- src/tests.rs
+++ src/tests.rs
@@ -1,26 +1,50 @@
 #![cfg(test)]
 use super::*;
-use nom::{
-    error::{Error, ErrorKind},
-    Err, IResult,
-};
-
+use nom::IResult;
+use nom_supreme::error::{BaseErrorKind, ErrorTree, Expectation};
 use test_case::test_case;
 
 #[test_case("2023-03-23", Ok(("", NaiveDate::from_ymd_opt(2023, 3, 23).unwrap())))]
 #[test_case("2023-04-011", Ok(("1", NaiveDate::from_ymd_opt(2023, 4, 1).unwrap())))]
 #[test_case(
     "2023-03-99",
-    Err(Err::Error(Error::new("2023-03-99", ErrorKind::Fail)))
-)]
-#[test_case(
-    "2023-03-XX",
-    Err(Err::Error(Error::new("XX", ErrorKind::TakeWhileMN)))
-)]
-#[test_case(
-    "freddyfish",
-    Err(Err::Error(Error::new("freddyfish", ErrorKind::TakeWhileMN)))
-)]
-fn test_date(s: &str, expected: IResult<&str, NaiveDate>) {
-    assert_eq!(date(s), expected);
+    Err(nom::Err::Error(ErrorTree::Base {
+            location: "2023-03-99",
+            kind: BaseErrorKind::External(Box::new(ParseError {
+                reason: ParseErrorReason::DateOutOfRange,
+            }))
+    })))]
+#[test_case("2023_04-011", Err(nom::Err::Error(ErrorTree::Base { location: "_04-011", kind: BaseErrorKind::Expected(Expectation::Tag("-")) })))]
+//#[test_case("2023_03-23", Err(Err::Error(Error::new("_03-23", ErrorKind::Tag))))]
+//#[test_case(
+//    "2023-03-XX",
+//    Err(Err::Error(Error::new("XX", ErrorKind::TakeWhileMN)))
+//)]
+//#[test_case(
+//    "freddyfish",
+//    Err(Err::Error(Error::new("freddyfish", ErrorKind::TakeWhileMN)))
+//)]
+fn test_date(s: &str, expected: IResult<&str, NaiveDate, ErrorTree<&str>>) {
+    match (date(s), expected) {
+        (Ok(actual), Ok(expected)) => assert_eq!(actual, expected),
+        (
+            Err(nom::Err::Error(ErrorTree::Base {
+                location: actual_location,
+                ..
+            })),
+            Err(nom::Err::Error(ErrorTree::Base {
+                location: expected_location,
+                ..
+            })),
+        ) => assert_eq!(actual_location, expected_location),
+        (actual, expected) => panic!("got {:?} expected {:?}", actual, expected),
+    }
 }
+
+/*
+#[test_case("2022-04-01 2023-03-31", Ok(Doc{ d1: NaiveDate::from_ymd_opt(2022, 4, 1).unwrap(), d2: NaiveDate::from_ymd_opt(2023, 3, 31).unwrap()}))]
+#[test_case("2022_04-01 2023-03-", Ok(Doc{ d1: NaiveDate::from_ymd_opt(2022, 4, 1).unwrap(), d2: NaiveDate::from_ymd_opt(2023, 3, 31).unwrap()}))]
+fn test_final_doc(s: &str, expected: Result<Doc, ()>) {
+    assert_eq!(final_doc(s), expected);
+}
+*/
