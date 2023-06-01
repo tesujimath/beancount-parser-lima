165a2ea8937dacce90c26be773f97333179e6842
Simon Guest <simon.guest@tesujimath.org>
2023-06-02 10:00:57 +1200

    Reorder parser functions

--- src/parser.rs
+++ src/parser.rs
@@ -19,64 +19,9 @@ use nom_supreme::{
     tag::complete::tag as sym, // beancount grammar has its own tag
 };
 
-#[derive(Debug)]
-enum ParseErrorReason {
-    DateMissingCentury,
-    DateOutOfRange,
-}
-
-#[derive(Debug)]
-pub struct ParseError {
-    reason: ParseErrorReason,
-}
-
-impl ParseError {
-    fn nom_error(location: &str, reason: ParseErrorReason) -> nom::Err<ErrorTree<&str>> {
-        nom::Err::Error(ErrorTree::Base {
-            location,
-            kind: BaseErrorKind::External(Box::new(ParseError { reason })),
-        })
-    }
-}
-
-impl Display for ParseError {
-    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
-        match self.reason {
-            ParseErrorReason::DateMissingCentury => write!(f, "date requires century"),
-            ParseErrorReason::DateOutOfRange => write!(f, "date out of range"),
-        }
-    }
-}
-
-impl Error for ParseError {}
-
-pub fn date(i0: &str) -> IResult<&str, NaiveDate, ErrorTree<&str>> {
-    fn date_sep(i: &str) -> IResult<&str, (), ErrorTree<&str>> {
-        one_of("-/")(i).map(|(s, _)| (s, ()))
-    }
-
-    let (i, (year, year_len)) = map_res(take_while1(|c: char| c.is_ascii_digit()), |s: &str| {
-        s.parse::<i32>().map(|y| (y, s.len()))
-    })(i0)?;
-    if year_len < 4 {
-        return Err(ParseError::nom_error(
-            i0,
-            ParseErrorReason::DateMissingCentury,
-        ));
-    }
-    let (i, _) = date_sep(i)?;
-    let (i, month) = map_res(take_while1(|c: char| c.is_ascii_digit()), |s: &str| {
-        s.parse::<u32>()
-    })(i)?;
-    let (i, _) = date_sep(i)?;
-    let (i, day) = map_res(take_while1(|c: char| c.is_ascii_digit()), |s: &str| {
-        s.parse::<u32>()
-    })(i)?;
-
-    match NaiveDate::from_ymd_opt(year, month, day) {
-        Some(d) => Ok((i, d)),
-        None => Err(ParseError::nom_error(i0, ParseErrorReason::DateOutOfRange)),
-    }
+/// Matches the `txn` keyword or a flag.
+pub fn txn(i: &str) -> IResult<&str, Flag, ErrorTree<&str>> {
+    alt((map(sym("txn"), |_| Flag::Asterisk), flag))(i)
 }
 
 /// Matches any flag, including asterisk or hash.
@@ -92,11 +37,6 @@ pub fn flag(i: &str) -> IResult<&str, Flag, ErrorTree<&str>> {
             FlagLetter::try_from(c).map(Flag::Letter)
         }),
     ))(i)
-}
-
-/// Matches the `txn` keyword or a flag.
-pub fn txn(i: &str) -> IResult<&str, Flag, ErrorTree<&str>> {
-    alt((map(sym("txn"), |_| Flag::Asterisk), flag))(i)
 }
 
 /// Matches zero or more quoted strings, optionally separated by whitespace.
@@ -140,6 +80,35 @@ pub fn tag_or_link_identifier(i: &str) -> IResult<&str, TagOrLinkIdentifier, Err
     )(i)
 }
 
+pub fn date(i0: &str) -> IResult<&str, NaiveDate, ErrorTree<&str>> {
+    fn date_sep(i: &str) -> IResult<&str, (), ErrorTree<&str>> {
+        one_of("-/")(i).map(|(s, _)| (s, ()))
+    }
+
+    let (i, (year, year_len)) = map_res(take_while1(|c: char| c.is_ascii_digit()), |s: &str| {
+        s.parse::<i32>().map(|y| (y, s.len()))
+    })(i0)?;
+    if year_len < 4 {
+        return Err(ParseError::nom_error(
+            i0,
+            ParseErrorReason::DateMissingCentury,
+        ));
+    }
+    let (i, _) = date_sep(i)?;
+    let (i, month) = map_res(take_while1(|c: char| c.is_ascii_digit()), |s: &str| {
+        s.parse::<u32>()
+    })(i)?;
+    let (i, _) = date_sep(i)?;
+    let (i, day) = map_res(take_while1(|c: char| c.is_ascii_digit()), |s: &str| {
+        s.parse::<u32>()
+    })(i)?;
+
+    match NaiveDate::from_ymd_opt(year, month, day) {
+        Some(d) => Ok((i, d)),
+        None => Err(ParseError::nom_error(i0, ParseErrorReason::DateOutOfRange)),
+    }
+}
+
 /// Matches a quoted string supporting embedded newlines and character escapes for `\\`, `\"`, `\n`, `\t`.
 pub fn string(i: &str) -> IResult<&str, String, ErrorTree<&str>> {
     fn string_content(i: &str) -> IResult<&str, String, ErrorTree<&str>> {
@@ -157,5 +126,36 @@ pub fn string(i: &str) -> IResult<&str, String, ErrorTree<&str>> {
 
     delimited(sym("\""), string_content, sym("\""))(i)
 }
+
+#[derive(Debug)]
+enum ParseErrorReason {
+    DateMissingCentury,
+    DateOutOfRange,
+}
+
+#[derive(Debug)]
+pub struct ParseError {
+    reason: ParseErrorReason,
+}
+
+impl ParseError {
+    fn nom_error(location: &str, reason: ParseErrorReason) -> nom::Err<ErrorTree<&str>> {
+        nom::Err::Error(ErrorTree::Base {
+            location,
+            kind: BaseErrorKind::External(Box::new(ParseError { reason })),
+        })
+    }
+}
+
+impl Display for ParseError {
+    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
+        match self.reason {
+            ParseErrorReason::DateMissingCentury => write!(f, "date requires century"),
+            ParseErrorReason::DateOutOfRange => write!(f, "date out of range"),
+        }
+    }
+}
+
+impl Error for ParseError {}
 
 mod tests;
