9ad3c2f9e4f25aab61be436b8c305bb2fd8f15a2
Simon Guest <simon.guest@tesujimath.org>
2023-06-26 17:24:26 +1200

    Chumsky expr, tests failing

--- src/parser/expr.rs
+++ src/parser/expr.rs
@@ -7,6 +7,65 @@ use {rust_decimal::Decimal, rust_decimal_macros::dec};
 #[cfg(test)]
 use test_case::test_case;
 
+/// Match an expression
+pub fn expr<'src>() -> impl Parser<'src, &'src str, Expr, extra::Err<Rich<'src, char, Span>>> {
+    product().foldl(
+        choice((
+            op('+').to(Expr::Add as fn(_, _) -> _),
+            op('-').to(Expr::Sub as fn(_, _) -> _),
+        ))
+        .then(product())
+        .repeated(),
+        |lhs, (op, rhs)| op(Box::new(lhs), Box::new(rhs)),
+    )
+}
+
+#[cfg(test)]
+#[test_case("1 + 2 *  3", "(1 + (2 * 3))")]
+#[test_case("1 + 2 *  3 / 4 - 5", "((1 + ((2 * 3) / 4)) - 5)")]
+#[test_case("72 / 2 / 3", "((72 / 2) / 3)")]
+#[test_case("10 - 1", "(10 - 1)")]
+#[test_case("10 - -2", "(10 - -2)")]
+fn expr_test(s: &str, expected: &str) {
+    assert_eq!(
+        expr().map(|x| format!("{:?}", x)).parse(s).into_result(),
+        Ok(expected.to_owned())
+    )
+}
+
+/// Match a product of factors
+pub fn product<'src>() -> impl Parser<'src, &'src str, Expr, extra::Err<Rich<'src, char, Span>>> {
+    factor().foldl(
+        choice((
+            op('*').to(Expr::Mul as fn(_, _) -> _),
+            op('/').to(Expr::Div as fn(_, _) -> _),
+        ))
+        .then(factor())
+        .repeated(),
+        |lhs, (op, rhs)| op(Box::new(lhs), Box::new(rhs)),
+    );
+
+    value().or(parens())
+}
+
+/// Match the specified operator
+pub fn op<'src>(c: char) -> impl Parser<'src, &'src str, (), extra::Err<Rich<'src, char, Span>>> {
+    just(c).ignored().padded()
+}
+
+/// Match a factor of an expression
+pub fn factor<'src>() -> impl Parser<'src, &'src str, Expr, extra::Err<Rich<'src, char, Span>>> {
+    value().or(parens())
+}
+
+/// Match a parenthesized expression
+pub fn parens<'src>() -> impl Parser<'src, &'src str, Expr, extra::Err<Rich<'src, char, Span>>> {
+    value()
+        .padded()
+        .delimited_by(just('('), just(')'))
+        .map(|x| Expr::Paren(Box::new(x)))
+}
+
 /// Match a single digit
 pub fn digit<'src>() -> impl Parser<'src, &'src str, char, extra::Err<Rich<'src, char, Span>>> {
     any().filter(char::is_ascii_digit)
