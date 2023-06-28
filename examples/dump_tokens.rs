3bacffb8785be796aca0d354f0c313e8b2a97f8b
Simon Guest <simon.guest@tesujimath.org>
2023-06-29 09:29:36 +1200

    dump_tokens takes filenames from CLI args

--- examples/dump_tokens.rs
+++ examples/dump_tokens.rs
@@ -1,14 +1,17 @@
+use std::env;
 use std::fs::File;
 use std::io::{self, prelude::*};
 
 use beancount_parser::dump_tokens;
 
 fn main() -> io::Result<()> {
-    let mut f = File::open("examples/data/example.beancount")?;
-    let mut buffer = String::new();
+    for arg in env::args().skip(1) {
+        let mut f = File::open(arg)?;
+        let mut buffer = String::new();
 
-    // read the whole file
-    f.read_to_string(&mut buffer)?;
-    dump_tokens(&buffer);
+        // read the whole file
+        f.read_to_string(&mut buffer)?;
+        dump_tokens(&buffer);
+    }
     Ok(())
 }
