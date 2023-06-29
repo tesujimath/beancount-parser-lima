983f1419b7c2f24626e35034e6cdb09e8e38ac29
Simon Guest <simon.guest@tesujimath.org>
2023-06-30 09:45:53 +1200

    Completed visible tokens for logos

--- src/lexer.rs
+++ src/lexer.rs
@@ -1,9 +1,15 @@
+use std::borrow::Cow;
+
 use super::*;
 use chrono::{NaiveDate, NaiveDateTime, NaiveTime};
 use logos::Logos;
 
 #[derive(Logos, Debug, PartialEq)]
-#[logos(error = LexerError, skip r"[ \t]+")] // Ignore this regex pattern between tokens
+#[logos(error = LexerError, skip r"[ \t\n]+")] // TODO don't skip on newline, need Eol and Indent tokens
+#[logos(subpattern account_type = r"[\p{Lu}\p{Lo}][\p{L}\p{N}\-]*")]
+#[logos(subpattern account_name = r"[\p{Lu}\p{Lo}\p{N}][\p{L}\p{N}\-]*")]
+#[logos(subpattern tag_or_link_identifier = r"[A-Za-z0-9\-_/.]+")]
+#[logos(subpattern key = r"[a-z][a-zA-Z0-9\-_]+")]
 pub enum Token<'a> {
     #[token("TRUE")]
     True,
@@ -104,6 +110,35 @@ pub enum Token<'a> {
 
     #[regex(r"\d{4}[\-/]\d{2}[\-/]\d{2}[ \t]+\d{1,2}:\d{2}(:\d{2})?", |lex| parse_datetime(lex.slice()))]
     DateTime(NaiveDateTime),
+
+    #[regex(r"(?&account_type)(:(?&account_name))+", |lex| parse_account(lex.slice()))]
+    Account(super::Account<'a>),
+
+    #[regex(r#""([^\\"]|\\.)*""#, |lex| {
+        let len = lex.slice().len();  
+        if len >= 16384 {
+            Err(LexerError::new("string too long"))
+        } else {
+            unescape_string_literal(&lex.slice()[1..len-1])
+        }
+    })]
+    StringLiteral(Cow<'a, str>),
+
+    #[regex(r"\d+(,\d{3})*(\.\d+)?", |lex| parse_number(lex.slice()))]
+    Number(Decimal),
+
+    #[regex(r"#(?&tag_or_link_identifier)", |lex| TagOrLinkIdentifier::try_from(&lex.slice()[1..]).map(super::Tag))]
+    Tag(super::Tag<'a>),
+
+    #[regex(r"\^(?&tag_or_link_identifier)", |lex| TagOrLinkIdentifier::try_from(&lex.slice()[1..]).map(super::Link))]
+    Link(super::Link<'a>),
+
+     // TODO only in trailing colon context
+    #[regex(r"(?&key)", |lex| Key::try_from(lex.slice()))]
+    Key(super::Key<'a>),
+
+    #[regex(r".", priority = 0, callback = |lex| println!("unrecognized token '{}' at {:?}", lex.slice(), lex.span()))]
+    Unrecognized,
 }
 
 // TODO remove this temporary diagnostic
@@ -141,6 +176,46 @@ fn parse_datetime(s: &str) -> Result<NaiveDateTime, LexerError> {
     Ok(NaiveDateTime::new(date, time))
 }
 
+fn parse_account(s: &str) -> Result<Account, LexerError> {
+    let mut account = s.split(':');
+    let account_type = account.by_ref().next().unwrap().parse::<AccountType>()?;
+    let account_names = account
+        .by_ref()
+        .map(AccountName::try_from)
+        .collect::<Result<Vec<AccountName>, _>>()?;
+    Ok(Account::new(
+        account_type,
+        NonEmpty::collect(account_names.into_iter()).unwrap(),
+    ))
+}
+
+fn unescape_string_literal(s: &str) -> Result<Cow<str>, LexerError> {
+    if s.contains('\\') {
+        // TODO be more careful here to check for malformed backslash escapes - maybe there's a crate for this?
+        Ok(Cow::Owned(
+            s
+                .replace("\\n", "\n")
+                .replace("\\t", "\t")
+                .replace("\\\"", "\"")
+                .replace("\\\\", "\\"),
+        ))
+    } else {
+        Ok(Cow::Borrowed(s))
+    }
+}
+
+fn parse_number(s: &str) -> Result<Decimal, LexerError> {
+    let result = if s.contains(',') {
+        let mut without_commas = s.to_string();
+        without_commas.retain(|c| c != ',');
+        FromStr::from_str(&without_commas)
+    } else {
+        FromStr::from_str(s)
+    };
+
+    result.map_err(|e: <rust_decimal::Decimal as std::str::FromStr>::Err| LexerError::new(e.to_string()))
+}
+
 #[derive(PartialEq, Clone, Debug)]
 pub struct LexerError {
     message: String,
@@ -168,16 +243,36 @@ impl Error for LexerError {}
 
 impl From<CurrencyError> for LexerError {
     fn from(e: CurrencyError) -> LexerError {
-        LexerError {
-            message: e.to_string(),
-        }
+        LexerError::new(e.to_string())
     }
 }
 
 impl From<FlagLetterError> for LexerError {
     fn from(e: FlagLetterError) -> LexerError {
-        LexerError {
-            message: e.to_string(),
-        }
+        LexerError::new(e.to_string())
+    }
+}
+
+impl From<AccountNameError> for LexerError {
+    fn from(e: AccountNameError) -> LexerError {
+        LexerError::new(e.to_string())
+    }
+}
+
+impl From<TagOrLinkIdentifierError> for LexerError {
+    fn from(e: TagOrLinkIdentifierError) -> LexerError {
+        LexerError::new(e.to_string())
+    }
+}
+
+impl From<KeyError> for LexerError {
+    fn from(e: KeyError) -> LexerError {
+        LexerError::new(e.to_string())
+    }
+}
+
+impl From<strum::ParseError> for LexerError {
+    fn from(e: strum::ParseError) -> LexerError {
+        LexerError::new(e.to_string())
     }
 }
