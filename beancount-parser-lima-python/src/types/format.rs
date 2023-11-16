9ba14de06270ebf87b216abfbfd1fca4d60dce0c
Simon Guest <simon.guest@tesujimath.org>
2023-11-16 16:07:56 +1300

    Print postings

--- beancount-parser-lima-python/src/types/format.rs
+++ beancount-parser-lima-python/src/types/format.rs
@@ -2,7 +2,8 @@ use super::*;
 use ::beancount_parser_lima::format::{
     double_quoted, format, plain, simple_format, NEWLINE_INDENT,
 };
-use pyo3::{pymethods, PyRef};
+use beancount_parser_lima::format::NEWLINE;
+use pyo3::{pymethods, PyRef, Python};
 use std::{
     borrow::Borrow,
     fmt::{self, Display, Formatter},
@@ -10,14 +11,22 @@ use std::{
 
 #[pymethods]
 impl Transaction {
-    fn __str__(self_: PyRef<'_, Self>) -> String {
+    fn __str__(self_: PyRef<'_, Self>, py: Python<'_>) -> String {
         let d = self_.as_ref();
 
-        format!("{}", Fmt(|f| fmt_transaction(self_.borrow(), &d.date, f)))
+        format!(
+            "{}",
+            Fmt(|f| fmt_transaction(self_.borrow(), py, &d.date, f))
+        )
     }
 }
 
-fn fmt_transaction(x: &Transaction, date: &Py<PyDate>, f: &mut Formatter<'_>) -> fmt::Result {
+fn fmt_transaction(
+    x: &Transaction,
+    py: Python<'_>,
+    date: &Py<PyDate>,
+    f: &mut Formatter<'_>,
+) -> fmt::Result {
     // write!(f, "{} {}", date, self.flag)?;
     write!(f, "{}", date)?;
     // write!(f, "Transaction")?;
@@ -27,34 +36,43 @@ fn fmt_transaction(x: &Transaction, date: &Py<PyDate>, f: &mut Formatter<'_>) ->
     // we prefer to show tags and links inline rather then line by line in metadata
     // metadata.fmt_tags_links_inline(f)?;
     // metadata.fmt_keys_values(f)?;
-    format(f, &x.postings, plain, NEWLINE_INDENT, Some(NEWLINE_INDENT))
+    for posting in &x.postings {
+        f.write_str(NEWLINE_INDENT)?;
+        fmt_posting(posting, py, f)?;
+    }
+    f.write_str(NEWLINE)
 }
 
 #[pymethods]
 impl Posting {
-    fn __str__(&self) -> String {
-        self.to_string()
+    fn __str__(&self, py: Python<'_>) -> String {
+        format!("{}", Fmt(|f| fmt_posting(self, py, f)))
     }
 }
 
-impl Display for Posting {
-    fn fmt(&self, f: &mut Formatter<'_>) -> fmt::Result {
-        // simple_format(f, self.flag, None)?;
+fn fmt_posting(x: &Posting, py: Python<'_>, f: &mut Formatter<'_>) -> fmt::Result {
+    // simple_format(f, self.flag, None)?;
 
-        // write!(
-        //     f,
-        //     "{}{}",
-        //     if self.flag.is_some() { " " } else { "" },
-        //     &self.account
-        // )?;
+    format(
+        f,
+        x.account.as_ref(py),
+        plain,
+        ":",  // TODO if self.flag.is_some() { " " } else { "" },
+        None, // TODO if self.flag.is_some() { " " } else { "" },
+    )?;
+    // write!(
+    //     f,
+    //     "{}{}",
+    //     "", // TODO if self.flag.is_some() { " " } else { "" },
+    //     &self.account
+    // )?;
 
-        // simple_format(f, &self.amount, Some(" "))?;
-        simple_format(f, self.currency.as_ref(), Some(" "))
-        // simple_format(f, &self.cost_spec, Some(" "))?;
-        // simple_format(f, &self.price_annotation, Some(" "))?;
+    // simple_format(f, &self.amount, Some(" "))?;
+    simple_format(f, x.currency.as_ref(), Some(" "))
+    // simple_format(f, &self.cost_spec, Some(" "))?;
+    // simple_format(f, &self.price_annotation, Some(" "))?;
 
-        // self.metadata.fmt(f)
-    }
+    // self.metadata.fmt(f)
 }
 
 struct Fmt<F>(pub F)
